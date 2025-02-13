6bb7399b514bfee31f615d251a3c5863b55ed645
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2025-02-13 15:06:34 +0100

    test of adding a new client

--- epic_events/main.py
+++ epic_events/main.py
@@ -3,14 +3,20 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "/.."))
 
-from epic_events.config import engine, Base
+from epic_events.config import engine, Base, SessionLocal
 from epic_events.models import Client, Contract, Event
+from epic_events.crud import add_client, get_db_session
 
 def setup_database():
     """Creates the database and tables."""
     Base.metadata.create_all(engine)
     print("Database updated with Contracts and Events.")
 
+def test_add_client():
+    session = get_db_session(SessionLocal)
+    add_client(session, "User BOT", "bot.com", "0611111111", "BOT LLC")
+
 if __name__ == "__main__":
-    setup_database()
+    # setup_database()
+    test_add_client()
 
