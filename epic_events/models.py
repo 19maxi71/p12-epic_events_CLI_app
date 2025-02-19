f99dd5825559205977b79ce3d6bfe5b9d7eb2ae4
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2025-02-19 22:56:17 +0100

    changes to models

--- epic_events/models.py
+++ epic_events/models.py
@@ -1,7 +1,20 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, Float, LargeBinary
 from sqlalchemy.orm import relationship
 from datetime import datetime, timezone
 from .config import Base
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)  # "Admin", "Commercial", "Support", "Gestion"
+
+    def __repr__(self):
+        return f"<Role {self.id} | {self.name}>"
+
+
 
 class Client(Base):
     __tablename__ = "clients" # table name in db
@@ -13,6 +26,7 @@ class Client(Base):
     company_name = Column(String, nullable=False)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    # rajouter le commercial
 
     def __repr__(self):
         return f"<Client {self.full_name} ({self.company_name})>"
@@ -23,7 +37,7 @@ class Contract(Base):
 
     id = Column(Integer, primary_key=True)
     client_id = Column(Integer, ForeignKey("clients.id"), nullable=False)
-    sales_contact = Column(String, nullable=False)
+    sales_contact = Column(String, nullable=False) # mettre id de commercial
     total_amount = Column(Float, nullable=False)
     amount_due = Column(Float, nullable=False)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
@@ -44,7 +58,7 @@ class Event(Base):
 
     id = Column(Integer, primary_key=True)
     contract_id = Column(Integer, ForeignKey("contracts.id"), nullable=False)
-    support_contact = Column(String, nullable=True)  # Assigned support staff
+    support_contact = Column(String, nullable=True)  # id de l'employé
     start_date = Column(DateTime, nullable=False)
     end_date = Column(DateTime, nullable=False)
     location = Column(String, nullable=False)
